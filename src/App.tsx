02644eb136d304295aac782c9ca90744fb64a1ef
farsana22 <farsanaa367@gmail.com>
2023-10-10 20:09:03 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
+import { Toaster } from 'react-hot-toast';
 import './App.css';
+import Login from './Login';
+import Signin from './Signin';
 import { Todo } from './Todo';
-
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-  <Todo/>
-)
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Todo />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signin" element={<Signin />} />
+        </Routes>
+      </div>
+          <Toaster
+            position="top-center"
+            reverseOrder={false}
+          />
+    </Router>
+  )
 
 }
 
--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,18 +1,20 @@
 import { useEffect, useState } from 'react';
+import nothingImg from './assets/nodata.png'
 
 interface todoType {
-    id: string;
-    title: string;
-    date: string;
-    isComplete: boolean | undefined;
-  }
+  id: string;
+  title: string;
+  date: string;
+  isComplete: boolean | undefined;
+}
 
-  export const Todo = () => {
+export const Todo = () => {
 
   const [todos, setTodos] = useState<todoType[]>([]);
   const [todo, setTodo] = useState<string>("");
-  const [todoStatus, setTodoStatus] = useState<boolean>();
-
+  const [todoStatus, setTodoStatus] = useState<boolean>(false);
+  console.log(setTodoStatus)
+  
   const generateUniqueId = () => {
     return new Date().getTime().toString();
   }
@@ -47,7 +49,7 @@ interface todoType {
   }
 
   const deleteTodo = (id: any) => {
-    const updatedTodos = todos.filter((todo:any) => todo.id !== id);
+    const updatedTodos = todos.filter((todo: any) => todo.id !== id);
     setTodos(updatedTodos);
     localStorage.setItem('todos', JSON.stringify(updatedTodos));
     countCompletedTodos()
@@ -56,7 +58,7 @@ interface todoType {
 
   //update the status of the todo
   const handleCompletion = (index: any) => {
-    const updatedTodos = todos.map((todo:any) => {
+    const updatedTodos = todos.map((todo: any) => {
       if (todo.id === index) {
         return { ...todo, isComplete: !todo.isComplete };
       }
@@ -71,103 +73,104 @@ interface todoType {
   const countAllTodos = () => todos.length;
 
   //get completed todo count 
-  const countCompletedTodos = () => todos.filter((todo:any) => todo.isComplete).length;
+  const countCompletedTodos = () => todos.filter((todo: any) => todo.isComplete).length;
 
 
 
 
 
   return (
-        <div className="App">
-          <div className="container">
-          </div>
-          <div className="todowrapper">
-            <div className="titleBox">
-              <span className="title">LoveTodo</span>
-            </div>
-            <div className="inputBox">
-              <input type="text" className='todoinput' onChange={(e: any) => handleChange(e)} value={todo} placeholder='Add a new task' />
-              <span className="send" onClick={addTodo} >
-                send
-              </span>
-            </div>
-            <div className="todoDisplayBox">
-              {todos.length != 0 &&
-                <div >
-                  <div className="info">
-                    <div className="infoWrapper">
-                      <span className="infoItem">Created Tasks: </span><div className="createdList">{countAllTodos()}</div>
-                    </div>
-                    <div className="infoWrapper">
-                      <span className="infoItem">Completed Tasks:</span><div className="createdList">{countCompletedTodos()}/{countAllTodos()}</div>
-                    </div>
-                  </div>
-                  <hr />
-                </div>
-              }
-    
-              {
-                todos.length === 0 ?
-    
-                  <div className="noDataCol">
-    
-                    {/* <img src={nothingImage} alt="hey" className='noData' /> */}
-                    <span className="dataMsg">You haven't added any todos!</span>
-                  </div>
-    
-                  :
-    
-                  <div className="todoItemCol">
-    
-                    {
-                      todos && todos.map((task) => (
-    
-                        <div className="todoItem" key={task?.id}>
-                          <div className="todoItemWrapper">
-                            <div className="checkbox">
-                              {task?.isComplete
-                                ?
-                                <span onClick={(e: any) => {
-                                  console.log("first")
-                                  e.preventDefault();
-                                  handleCompletion(task?.id)
-                                }} className="material-symbols-outlined check">
-                                  check_circle
-                                </span>
-                                :
-                                <span onClick={(e: any) => {
-                                  console.log("first")
-                                  e.preventDefault();
-                                  handleCompletion(task?.id)
-                                }} className="material-symbols-outlined check">
-                                  radio_button_unchecked
-                                </span>
-                              }
-                            </div>
-                            <div className="todoDetails">
-                              <span className="todoTitle">{task.title}</span>
-                              <span className="todoTime">{task.date}</span>
-                            </div>
-                            <div className="action">
-                              <span className="material-symbols-outlined check" onClick={(e: any) => {
-                                e.preventDefault()
-                                deleteTodo(task?.id)
-                              }}>
-                                delete
-                              </span>
-                            </div>
-                          </div>
-                        </div>
-    
-                      ))
-                    }
-                  </div>
-              }
-            </div>
-          </div>
-          <div className="love">
-            <span className="author">Made with <span className="emoji">❤️</span> Farsana</span>
-          </div>
+    <div className="App">
+      <div className="container">
+      </div>
+      <div className="todowrapper">
+        <div className="titleBox">
+          <span className="title">Todo</span>
+          <button className='logout'>Logout</button>
         </div>
+        <div className="inputBox">
+          <input type="text" className='todoinput' onChange={(e: any) => handleChange(e)} value={todo} placeholder='Add a new task' />
+          <span className="material-symbols-outlined send" onClick={addTodo} >
+            send
+          </span>
+        </div>
+        <div className="todoDisplayBox">
+          {todos.length != 0 &&
+            <div >
+              <div className="info">
+                <div className="infoWrapper">
+                  <span className="infoItem">Created Tasks: </span><div className="createdList">{countAllTodos()}</div>
+                </div>
+                <div className="infoWrapper">
+                  <span className="infoItem">Completed Tasks:</span><div className="createdList">{countCompletedTodos()}/{countAllTodos()}</div>
+                </div>
+              </div>
+              <hr />
+            </div>
+          }
+
+          {
+            todos.length === 0 ?
+
+              <div className="noDataCol">
+
+                <img src={nothingImg} alt="hey" className='noData' />
+                <span className="dataMsg">You haven't added any todos!</span>
+              </div>
+
+              :
+
+              <div className="todoItemCol">
+
+                {
+                  todos && todos.map((task) => (
+
+                    <div className="todoItem" key={task?.id}>
+                      <div className="todoItemWrapper">
+                        <div className="checkbox">
+                          {task?.isComplete
+                            ?
+                            <span onClick={(e: any) => {
+                              console.log("first")
+                              e.preventDefault();
+                              handleCompletion(task?.id)
+                            }} className="material-symbols-outlined check">
+                              check_circle
+                            </span>
+                            :
+                            <span onClick={(e: any) => {
+                              console.log("first")
+                              e.preventDefault();
+                              handleCompletion(task?.id)
+                            }} className="material-symbols-outlined check">
+                              radio_button_unchecked
+                            </span>
+                          }
+                        </div>
+                        <div className="todoDetails">
+                          <span className="todoTitle">{task.title}</span>
+                          <span className="todoTime">{task.date}</span>
+                        </div>
+                        <div className="action">
+                          <span className="material-symbols-outlined check" onClick={(e: any) => {
+                            e.preventDefault()
+                            deleteTodo(task?.id)
+                          }}>
+                            delete
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+
+                  ))
+                }
+              </div>
+          }
+        </div>
+      </div>
+      <div className="love">
+        <span className="author">Made with <span className="emoji">❤️</span> by Farsana</span>
+      </div>
+    </div>
   )
 }
